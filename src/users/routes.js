b5a433b2e1bca163f0f2ab8f228cf471a50f414c
deergod <CommonTroll@live.co.uk>
2023-05-03 14:52:42 +0100

    adding the routes for users feature

--- src/users/routes.js
+++ src/users/routes.js
@@ -2,12 +2,12 @@ const { Router } = require("express");
 const userRouter = Router();
 
 const { registerUser, loginUser } = require("./controllers");
-const { hashPass, checkPass, checkToken } = require("../middleware");
+const { hashPass, comparePass, tokenCheck } = require("../middleware");
 
 userRouter.post("/users/register", hashPass, registerUser);
 
-userRouter.post("/users/login", checkPass, loginUser);
+userRouter.post("/users/login", comparePass, loginUser);
 
-userRouter.get("/users/authcheck", checkToken, loginUser);
+userRouter.get("/users/authcheck", tokenCheck, loginUser);
 
 module.exports = userRouter;
