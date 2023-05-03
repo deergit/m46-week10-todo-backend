728de62fd81fe294905f0b56086850311d1a2b23
bethNugent <bethnugent94@hotmail.co.uk>
2023-05-03 15:54:37 +0100

    added routes and models

--- src/server.js
+++ src/server.js
@@ -3,16 +3,27 @@ const express = require("express");
 
 const port = process.env.PORT || 5001;
 
+const User = require("./users/model");
+const DoneTodo = require("./doneTodos/model");
+const ActiveTodo = require("./activeTodos/model");
+
 const userRouter = require("./users/routes");
-const User = require("./users/model")
+const doneTodoRouter = require("./doneTodos/routes");
+const actTodoRouter = require("./activeTodos/routes");
+
+
 
 const app = (express());
 
 const syncTables = () => {
-    // User.sync()
+    User.sync()
+    DoneTodo.sync();
+    ActiveTodo.sync();
 }
 
-// app.use(userRouter)
+app.use(userRouter);
+app.use(doneTodoRouter);
+app.use(actTodoRouter);
 
 app.get("/health", (req, res) => {
     res.status(200).json({ message: "api is working" })
