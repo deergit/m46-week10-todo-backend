74898f278bf89fd8314fbeb294aa07106f492666
bethNugent <bethnugent94@hotmail.co.uk>
2023-05-04 10:54:12 +0100

    added DoneTodos controllers for addDoneTodo and deleteDoneTodo

---
+++ src/doneTodos/controllers.js
@@ -0,0 +1,35 @@
+const DoneTodo = require("./model");
+const jwt = require("jsonwebtoken");
+
+
+const addDoneTodo = async (req, res) => {
+    try {
+      console.log(req.body)
+  
+      const todo = await DoneTodo.create(req.body);
+  
+      res.status(201).json({ message: "success", todo: { doneTodo: req.body.doneTodo } });
+    } catch (error) {
+      res.status(501).json({ errorMessage: error.message, error: error });
+    }
+  };
+
+
+const deleteDoneTodo = async (req, res) => {
+    try{
+        console.log(req.body)
+
+        const delTodo = await DoneTodo.destroy(req.body);
+
+        res.status(201).json({ message: "success", delTodo: delTodo });
+    } catch (error) {
+      res.status(501).json({ errorMessage: error.message, error: error });
+    }        
+}
+
+
+
+  module.exports = {
+    addDoneTodo,
+    deleteDoneTodo
+  }
